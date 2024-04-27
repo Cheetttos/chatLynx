7fdb01403d173e92cb2a7b927d2f3497e12736c8
Cheetttos <20030425@itcelaya.edu.mx>
2024-04-27 09:52:45 -0600

    primer commit

---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'ChatLynx',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text("App Bar"),
+        ),
+        body: const Center(
+          child: Text("Hola"),
+        ),
+      ),
+    );
+  }
+}
